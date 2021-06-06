97396c85abbd1fdd5249d0cf72889158676eff9c
uellenberg <JonahUellenberg@hotmail.com>
2021-06-05 18:44:04 -0700

    Handle node_modules on modules correctly

--- build-modules.js
+++ build-modules.js
@@ -1,7 +1,14 @@
 const child_process = require("child_process");
 const fs = require("fs");
 const Path = require("path");
+const ncp = require("ncp").ncp;
+
+ncp.limit = 16;
 
 for (let module of fs.readdirSync("./modules")) {
+  child_process.exec("npm i", {cwd: Path.join("./modules", module)}, () => {});
+
+  ncp(Path.join("./modules", module, "node_modules"), "node_modules");
+
   child_process.exec("tsc", {cwd: Path.join("./modules", module)}, () => {});
 }
