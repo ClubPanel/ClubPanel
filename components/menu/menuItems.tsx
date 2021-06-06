ed5298071aead25506624bde14b45dcb1ef83a61
uellenberg <JonahUellenberg@hotmail.com>
2021-06-06 09:16:00 -0700

    Allow menu items to be customized

--- components/menu/menuItems.tsx
+++ components/menu/menuItems.tsx
@@ -2,10 +2,36 @@ import React, { Component } from "react";
 import MenuCategory from "./menuCategory";
 import MenuLink from "./menuLink";
 
+export interface IMenuLink {
+  text: string;
+  url: string;
+  aria: string;
+}
+
+const links: Record<string, IMenuLink[]> = {};
+
+export const RegisterMenuLink = (category: string, link?: IMenuLink) => {
+  if(!link && !links.hasOwnProperty(category)) {
+    links[category] = [];
+  }
+
+  if(link) links[category].push(link);
+};
+
 const MenuItems = () : JSX.Element => {
+  const list: JSX.Element[] = [];
+
+  for (const key of Object.keys(links)) {
+    list.push(<MenuCategory text={key} />);
+
+    for(const link of links[key]){
+      list.push(<MenuLink text={link.text} url={link.url}  aria={link.aria}/>);
+    }
+  }
+
   return (
     <>
-      <MenuCategory text="Activities"/>
+      {list}
     </>
   );
 };
