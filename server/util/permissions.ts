600edf378f9c36726b71a50005d6bf495dfe693f
uellenberg <JonahUellenberg@hotmail.com>
2021-06-26 17:07:32 -0700

    Force the owner to have the admin permission

--- server/util/permissions.ts
+++ server/util/permissions.ts
@@ -2,6 +2,9 @@ export const hasPermission = (permissions: string[], permission: string | string
   if(!permission) return true;
   if(!permissions) return false;
 
+  //Force the owner to have the admin permission.
+  if(permissions.includes("owner") && !permissions.includes("admin")) permissions.push("admin");
+
   if(typeof permission === "string") return permissions.includes(permission);
 
   for (const item of permission) {
