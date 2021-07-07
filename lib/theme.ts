90f745f584398310a820b87cc7f7000f35f1670d
uellenberg <JonahUellenberg@hotmail.com>
2021-07-06 18:13:29 -0700

    Improve theme

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,11 +1,34 @@
-import {extendTheme} from "@chakra-ui/react";
+import {extendTheme, withDefaultProps} from "@chakra-ui/react";
 
 export const theme = extendTheme({
+  colors: {
+    dark: {
+      100: "#373d4e",
+      200: "#353e4c",
+      300: "#303944",
+      400: "#29303b",
+      500: "#222831",
+      600: "#1d232a",
+      700: "#1a1f26",
+      800: "#111318",
+      900: "#12151a",
+    }
+  },
   styles: {
     global: {
       body: {
-        bg: "#222831",
+        bg: "dark.500",
         color: "white"
+      }
+    }
+  },
+  components: {
+    Drawer: {
+      baseStyle: {
+        dialog: {
+          bg: "dark.700",
+          color: "white"
+        }
       }
     }
   },
