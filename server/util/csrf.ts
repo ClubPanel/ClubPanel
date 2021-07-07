f415a7be6e9f67f6df0b4a354b7de7560487b82a
uellenberg <JonahUellenberg@hotmail.com>
2021-07-06 17:16:15 -0700

    Add CSRF protection

---
+++ server/util/csrf.ts
@@ -0,0 +1,24 @@
+import crypto from "crypto";
+import express from "express";
+import session from "express-session";
+
+declare module "express-session" {
+  export interface SessionData {
+    csrfToken?: string;
+  }
+}
+
+export const requireCSRF = () => {
+  return (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if(req.body?.hasOwnProperty?.("csrf") && req.body?.csrf === getCSRF(req.session)) next();
+    else res.status(403).send("A CSRF token was not provided, or it is invalid.");
+  };
+};
+
+export const getCSRF = (session: session.SessionData) => {
+  if(!session.csrfToken) {
+    session.csrfToken = crypto.randomBytes(32).toString("hex");
+  }
+
+  return session.csrfToken;
+};
